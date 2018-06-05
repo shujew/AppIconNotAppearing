ab3dc5cc0ce29565d8d7d918e8e1439054525096
Shuaib Jewon <shuaib.jewon@slader.com>
2018-06-05 19:02:12 -0400

    initial commit

---
+++ AppIconNotAppearing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppIconNotAppearing
+//
+//  Created by Shuaib Jewon on 6/5/18.
+//  Copyright © 2018 shujew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
